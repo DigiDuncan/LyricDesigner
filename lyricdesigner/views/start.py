9b5cc21c904ad0d45d693042e5b55cb119b988e9
DigiDuncan <digiduncan@gmail.com>
2021-12-17 02:35:53 -0500

    more test code

--- lyricdesigner/views/start.py
+++ lyricdesigner/views/start.py
@@ -13,6 +13,7 @@ class StartView(arcade.View):
         self.progress_bar.center_x = self.window.width / 2
         self.progress_bar.center_y = 200
         self.frame = 0
+        self.last_dump = 0
 
     def on_show(self):
         arcade.set_background_color(arcade.color.BLACK)
@@ -22,13 +23,21 @@ class StartView(arcade.View):
         self.current_time = round(self.current_time, 2)
         self.progress_bar.progress += (delta_time / 60)
         self.frame += 1
-        if self.frame % 60 == 0:
+        self.last_dump += delta_time
+        if self.last_dump > 1:
             print(arcade.get_fps())
+            self.last_dump = 0
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text("Test Program", self.window.width / 2, self.window.height - 260, arcade.color.YELLOW, 96, anchor_x="center", bold=True)
-        arcade.draw_text(f"{self.current_time:.2f}", self.window.width / 2, self.window.height - 400, arcade.color.YELLOW, 60, anchor_x="center")
+        y = self.window.height - 260
+        txt1 = arcade.Text("Test Program", self.window.width / 2, y, arcade.color.YELLOW, 96, anchor_x="center", bold=True)
+        txt2 = arcade.Text(f"{self.current_time:.2f}", self.window.width / 2, y, arcade.color.YELLOW, 60, anchor_x="center", anchor_y="top")
+        # txt2.y -= txt2._label.content_height
+        arcade.draw_line(0, y, self.window.width, y, arcade.color.BLUE)
+        arcade.draw_lrtb_rectangle_outline(txt2.x - txt2._label.content_width / 2, txt2.x + txt2._label.content_width / 2, txt2.y, txt2.y - txt2._label.content_height, arcade.color.RED)
+        txt1.draw()
+        txt2.draw()
         arcade.draw_text(self.current_text, self.window.width / 2, 250, arcade.color.WHITE, 32, anchor_x="center")
         self.progress_bar.draw()
 
